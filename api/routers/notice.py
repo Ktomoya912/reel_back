84e70c0fec172d2fb3420395d154c2f998d81bd4
Ktomoya912 <250304s@ugs.kochi-tech.ac.jp>
2023-12-27 23:52:39 +0900

    メッセージを作成できるようにした

--- api/routers/notice.py
+++ api/routers/notice.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.cruds.message as message_crud
+import api.schemas.message as message_schema
 import api.schemas.user as user_schema
 from api.db import get_db
 
@@ -10,17 +11,31 @@ from .auth import get_current_user
 router = APIRouter(prefix="/notices", tags=["notices"])
 
 
-@router.get("/notifications")
+@router.get("/")
 async def get_notifications(
     db: AsyncSession = Depends(get_db),
     current_user: user_schema.User = Depends(get_current_user),
-):
+) -> list[list[message_schema.Message]]:
     job_messages = await message_crud.get_messages(db, current_user.id, "J")
     event_messages = await message_crud.get_messages(db, current_user.id, "E")
     return [
         event_messages,
         job_messages,
     ]
+
+
+@router.post("/", response_model=message_schema.Message)
+async def create_notification(
+    message_create: message_schema.MessageCreate,
+    db: AsyncSession = Depends(get_db),
+    current_user: user_schema.User = Depends(get_current_user),
+):
+    message = await message_crud.create_message(db, message_create)
+    await message_crud.send_message(db, message_create.user_list, message.id)
+    await db.refresh(message)
+    return message
+
+
 @router.post("/{message_id}/read")
 async def read_notification(
     message_id: int,
--- api/schemas/message.py
+++ api/schemas/message.py
@@ -1,14 +1,18 @@
+from typing import List
+
 from pydantic import BaseModel, Field
 
 
 class MessageBase(BaseModel):
-    title: str
-    message: str = Field(..., max_length=1000)
-    type: str = Field(..., max_length=1)
+    title: str = Field(..., max_length=255, description="タイトル")
+    message: str = Field(..., max_length=1000, description="メッセージ")
+    type: str = Field(
+        ..., max_length=1, description="J:ジョブ, E:イベント", examples=["J", "E"]
+    )
 
 
 class MessageCreate(MessageBase):
-    pass
+    user_list: List[int]
 
 
 class Message(MessageBase):
