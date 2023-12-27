3dd4fedbc08b682957bc7c4822a848288c8a7557
Ktomoya912 <250304s@ugs.kochi-tech.ac.jp>
2023-12-27 23:48:16 +0900

    to_dictで文字列を帰すようにした

--- api/db.py
+++ api/db.py
@@ -45,7 +45,7 @@ class BaseModel(Base):
         )
 
     def to_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
 
 
 async def get_db():
