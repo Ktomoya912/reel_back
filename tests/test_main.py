564e0963d94890bd68b2db5454709388f23e57a3
Ktomoya912 <250304s@ugs.kochi-tech.ac.jp>
2024-01-11 16:38:40 +0900

    テストコード記述

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,6 +15,31 @@ class TestUser:
         )
         assert response.status_code == 200, response.text
 
+    def test_read_user(self, general_client: TestClient, api_path: str):
+        response = general_client.get(f"{api_path}/users/")
+        assert response.status_code == 200, response.text
+        response_json = response.json()
+        assert len(response_json) == 1, response_json
+        assert response_json[0]["username"] == "username", response_json
+
+    def test_update_user(self, general_client: TestClient, api_path: str):
+        response = general_client.put(
+            f"{api_path}/users/1",
+            json={
+                "username": "username2",
+                "password": "password2",
+                "email": "email@sample.com",
+                "sex": "f",
+                "birthday": "2021-01-02",
+            },
+        )
+        assert response.status_code == 200, response.text
+        response_json = response.json()
+        assert response_json["username"] == "username2", response_json
+        assert response_json["email"] == "email@sample.com", response_json
+        assert response_json["sex"] == "f", response_json
+        assert response_json["birthday"] == "2021-01-02", response_json
+
 
 class TestPlan:
     def test_create_plan(
@@ -40,3 +65,10 @@ class TestPlan:
         )
         assert response.status_code == 200, response.text
         assert response.json()["name"] == "テストプラン"
+
+    def test_read_plan(self, general_client: TestClient, api_path: str):
+        response = general_client.get(f"{api_path}/plans/")
+        assert response.status_code == 200, response.text
+        response_json = response.json()
+        assert len(response_json) == 1, response_json
+        assert response_json[0]["name"] == "テストプラン", response_json
