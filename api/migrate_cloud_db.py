291f21c4509f19065d310ebe87d8c89c2c2020f2
Ktomoya912 <250304s@ugs.kochi-tech.ac.jp>
2024-01-16 21:43:14 +0900

    環境変数を読むように変更

--- api/migrate_cloud_db.py
+++ api/migrate_cloud_db.py
@@ -1,9 +1,11 @@
+from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import InternalError, OperationalError
 
 from api.db import DB_HOST, DB_PASSWORD, DB_PORT, DB_USER, Base  # noqa F401
 from api.models import company, event, job, message, plan, tag, user  # noqa F401
 
+load_dotenv()
 DB_URL = f"""mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:
 {DB_PORT}/?charset=utf8"""
 
